390da184d05c4c853bb51d5632d2c6515727b070
ATEMMETA <atem.86796@gmail.com>
2025-03-18 21:41:11 -0500

    Update IconBox.tsx

--- src/components/icons/IconBox.tsx
+++ src/components/icons/IconBox.tsx
@@ -1,17 +1,21 @@
-'use client'
+'use client';
+import React from 'react';
 import { Flex } from '@chakra-ui/react';
 
-export default function IconBox(props: { icon: JSX.Element | string; [x: string]: any }) {
-	const { icon, ...rest } = props;
+export default function IconBox(props: { 
+  icon: React.JSX.Element | string; // Change JSX.Element to React.JSX.Element
+  [x: string]: any 
+}): React.JSX.Element { // Add return type for good measure
+  const { icon, ...rest } = props;
 
-	return (
-        <Flex
-            alignItems={'center'}
-            justifyContent={'center'}
-            borderRadius={'50%'}
-            {...rest}
-        >
-            {icon}
-        </Flex>
-    );
+  return (
+    <Flex
+      alignItems={'center'}
+      justifyContent={'center'}
+      borderRadius={'50%'}
+      {...rest}
+    >
+      {icon}
+    </Flex>
+  );
 }
