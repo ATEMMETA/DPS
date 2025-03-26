8c01ca90570b03dda632ecec9a1f6d88d08d3ab2
ATEMMETA <atem.86796@gmail.com>
2025-03-26 15:59:23 -0500

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,8 +5,8 @@ import {
   MdLock,
   MdLayers,
   MdAutoAwesome,
-  MdOutlineManageAccounts,
   MdChat,
+  MdOutlineManageAccounts,
 } from 'react-icons/md';
 import { IoMdPerson } from 'react-icons/io';
 import { LuHistory } from 'react-icons/lu';
@@ -44,6 +44,7 @@ const routes: IRoute[] = [
     icon: <Icon as={MdChat} width="20px" height="20px" color="inherit" />,
     collapse: false,
   },
+  // Keep the rest disabled for future features
   {
     name: 'All Templates',
     disabled: true,
@@ -65,21 +66,9 @@ const routes: IRoute[] = [
     icon: <Icon as={MdFileCopy} width="20px" height="20px" color="inherit" />,
     collapse: true,
     items: [
-      {
-        name: 'Prompt Page',
-        layout: '/others',
-        path: '/prompt',
-      },
-      {
-        name: 'Register',
-        layout: '/others',
-        path: '/register',
-      },
-      {
-        name: 'Sign In',
-        layout: '/others',
-        path: '/sign-in',
-      },
+      { name: 'Prompt Page', layout: '/others', path: '/prompt' },
+      { name: 'Register', layout: '/others', path: '/register' },
+      { name: 'Sign In', layout: '/others', path: '/sign-in' },
     ],
   },
   {
@@ -89,26 +78,10 @@ const routes: IRoute[] = [
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
     collapse: true,
     items: [
-      {
-        name: 'All Templates',
-        layout: '/admin',
-        path: '/all-admin-templates',
-      },
-      {
-        name: 'New Template',
-        layout: '/admin',
-        path: '/new-template',
-      },
-      {
-        name: 'Edit Template',
-        layout: '/admin',
-        path: '/edit-template',
-      },
-      {
-        name: 'Users Overview',
-        layout: '/admin',
-        path: '/overview',
-      },
+      { name: 'All Templates', layout: '/admin', path: '/all-admin-templates' },
+      { name: 'New Template', layout: '/admin', path: '/new-template' },
+      { name: 'Edit Template', layout: '/admin', path: '/edit-template' },
+      { name: 'Users Overview', layout: '/admin', path: '/overview' },
     ],
   },
   {
@@ -116,12 +89,7 @@ const routes: IRoute[] = [
     disabled: true,
     path: '/settings',
     icon: (
-      <Icon
-        as={MdOutlineManageAccounts}
-        width="20px"
-        height="20px"
-        color="inherit"
-      />
+      <Icon as={MdOutlineManageAccounts} width="20px" height="20px" color="inherit" />
     ),
     invisible: true,
     collapse: false,
@@ -150,6 +118,7 @@ const routes: IRoute[] = [
     invisible: true,
     collapse: false,
   },
+  // Prompt Pages
   {
     name: 'Essay Generator',
     disabled: true,
@@ -194,37 +163,4 @@ const routes: IRoute[] = [
     name: 'Instagram Caption',
     disabled: true,
     path: '/caption',
-    icon: <Icon as={IoMdPerson} width="20px" height="20px" color="inherit" />,
-    invisible: true,
-    collapse: false,
-  },
-  {
-    name: 'FAQs Content',
-    disabled: true,
-    path: '/faq',
-    icon: <Icon as={IoMdPerson} width="20px" height="20px" color="inherit" />,
-    invisible: true,
-    collapse: false,
-  },
-  {
-    name: 'Product Name Generator',
-    disabled: true,
-    path: '/name-generator',
-    icon: <Icon as={IoMdPerson} width="20px" height="20px" color="inherit" />,
-    invisible: true,
-    collapse: false,
-  },
-  {
-    name: 'SEO Keywords',
-    disabled: true,
-    path: '/seo-keywords',
-    icon: <Icon as={IoMdPerson} width="20px" height="20px" color="inherit" />,
-    invisible: true,
-    collapse: false,
-  },
-  {
-    name: 'Review Responder',
-    disabled: true,
-    path: '/review-responder',
-    icon: <Icon as={IoMdPerson} width="20px" height="20px" color="inherit" />,
-    invisible: true,
+    icon: <
