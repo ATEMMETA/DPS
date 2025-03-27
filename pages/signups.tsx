161372c6e0cfe94a2e35b2c4720849d67b072c5b
ATEMMETA <atem.86796@gmail.com>
2025-03-27 12:56:01 -0500

    Update signups.tsx

--- pages/signups.tsx
+++ pages/signups.tsx
@@ -1,24 +1,72 @@
-import React, { FormEvent } from "react"; // Add FormEvent for submit
-// Rest of imports unchanged (Chakra, icons, components, assets)
+import React, { FormEvent, useState } from "react";
+import {
+  Flex,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Switch,
+  DarkMode,
+  Text,
+  useToast, // Import useToast
+} from "@chakra-ui/react";
+import GradientBorder from "components/GradientBorder/GradientBorder";
 
-function SignUp() {
+interface SignUpProps {}
+
+function SignUp({}: SignUpProps) {
   const titleColor = "white";
-  const textColor = "gray.400";
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false); // Loading state
+  const toast = useToast(); // Initialize useToast
 
   const handleSignUp = async (e: FormEvent) => {
     e.preventDefault();
-    console.log("Sign Up:", { name, email, password });
-    // TODO: Add auth logic (e.g., API call to /api/signup)
+    setLoading(true); // Start loading
+
+    try {
+      const response = await fetch("/api/signup", { // Replace with your API endpoint
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Sign-up failed");
+      }
+
+      toast({
+        title: "Sign-up successful",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+
+      // Redirect user or clear form
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error: any) {
+      toast({
+        title: "Sign-up error",
+        description: error.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } finally {
+      setLoading(false); // Stop loading
+    }
   };
 
   return (
     <Flex position="relative" overflow={{ lg: "hidden" }}>
-      {/* ... Existing Flex structure ... */}
       <FormControl as="form" onSubmit={handleSignUp}>
-        {/* Name */}
         <FormLabel color={titleColor} ms="4px" fontSize="sm" fontWeight="normal">
           Name
         </FormLabel>
@@ -39,10 +87,9 @@ function SignUp() {
             onChange={(e) => setName(e.target.value)}
           />
         </GradientBorder>
-        {/* Email */}
         <FormLabel color={titleColor} ms="4px" fontSize="sm" fontWeight="normal">
           Email
-       (FormLabel>
+        </FormLabel>
         <GradientBorder mb="24px" h="50px" w={{ base: "100%", lg: "fit-content" }} borderRadius="20px">
           <Input
             color={titleColor}
@@ -60,7 +107,6 @@ function SignUp() {
             onChange={(e) => setEmail(e.target.value)}
           />
         </GradientBorder>
-        {/* Password */}
         <FormLabel color={titleColor} ms="4px" fontSize="sm" fontWeight="normal">
           Password
         </FormLabel>
@@ -81,7 +127,6 @@ function SignUp() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </GradientBorder>
-        {/* Remember Me */}
         <FormControl display="flex" alignItems="center" mb="24px">
           <DarkMode>
             <Switch id="remember-login" colorScheme="brand" me="10px" />
@@ -99,367 +144,14 @@ function SignUp() {
           h="45"
           mb="20px"
           mt="20px"
+          isLoading={loading} // Show loading state
         >
           SIGN UP
         </Button>
       </FormControl>
-      {/import React from "react";
-
-// Chakra imports
-import {
-  Box,
-  Flex,
-  Button,
-  FormControl,
-  FormLabel,
-  HStack,
-  Input,
-  Link,
-  Switch,
-  Text,
-  Icon,
-  DarkMode,
-} from "@chakra-ui/react";
-
-// Icons
-import { FaApple, FaFacebook, FaGoogle } from "react-icons/fa";
-// Custom Components
-import AuthFooter from "components/Footer/AuthFooter";
-import GradientBorder from "components/GradientBorder/GradientBorder";
-
-// Assets
-import signUpImage from "assets/img/signUpImage.png";
-
-function SignUp() {
-  const titleColor = "white";
-  const textColor = "gray.400";
-
-  return (
-    <Flex position='relative' overflow={{ lg: "hidden" }}>
-      <Flex
-        flexDirection='column'
-        h={{ sm: "initial", md: "unset" }}
-        w={{ base: "90%" }}
-        maxW='1044px'
-        mx='auto'
-        justifyContent='space-between'
-        pt={{ sm: "100px", md: "0px" }}
-        me={{ base: "auto", lg: "50px", xl: "auto" }}>
-        <Flex
-          alignItems='center'
-          justifyContent='start'
-          style={{ userSelect: "none" }}
-          flexDirection='column'
-          mx={{ base: "auto", lg: "unset" }}
-          ms={{ base: "auto", lg: "auto" }}
-          mb='50px'
-          w={{ base: "100%", md: "50%", lg: "42%" }}>
-          <Flex
-            direction='column'
-            textAlign='center'
-            justifyContent='center'
-            align='center'
-            mt={{ base: "60px", md: "140px", lg: "200px" }}
-            mb='50px'>
-            <Text
-              fontSize='4xl'
-              lineHeight='39px'
-              color='white'
-              fontWeight='bold'>
-              Welcome!
-            </Text>
-            <Text
-              fontSize='md'
-              color='white'
-              fontWeight='normal'
-              mt='10px'
-              w={{ base: "100%", md: "90%", lg: "90%", xl: "80%" }}>
-              Use these awesome forms to login or create new account in your
-              project for free.
-            </Text>
-          </Flex>
-          <GradientBorder p='2px' me={{ base: "none", lg: "30px", xl: "none" }}>
-            <Flex
-              background='transparent'
-              borderRadius='30px'
-              direction='column'
-              p='40px'
-              minW={{ base: "unset", md: "430px", xl: "450px" }}
-              w='100%'
-              mx={{ base: "0px" }}
-              bg={{
-                base: "rgb(19,21,56)",
-              }}>
-              <Text
-                fontSize='xl'
-                color={textColor}
-                fontWeight='bold'
-                textAlign='center'
-                mb='22px'>
-                Register With
-              </Text>
-              <HStack spacing='15px' justify='center' mb='22px'>
-                <GradientBorder borderRadius='15px'>
-                  <Flex
-                    _hover={{ filter: "brightness(120%)" }}
-                    transition='all .25s ease'
-                    cursor='pointer'
-                    justify='center'
-                    align='center'
-                    bg='rgb(19,21,54)'
-                    w='71px'
-                    h='71px'
-                    borderRadius='15px'>
-                    <Link href='#'>
-                      <Icon
-                        color={titleColor}
-                        as={FaFacebook}
-                        w='30px'
-                        h='30px'
-                        _hover={{ filter: "brightness(120%)" }}
-                      />
-                    </Link>
-                  </Flex>
-                </GradientBorder>
-                <GradientBorder borderRadius='15px'>
-                  <Flex
-                    _hover={{ filter: "brightness(120%)" }}
-                    transition='all .25s ease'
-                    cursor='pointer'
-                    justify='center'
-                    align='center'
-                    bg='rgb(19,21,54)'
-                    w='71px'
-                    h='71px'
-                    borderRadius='15px'>
-                    <Link href='#'>
-                      <Icon
-                        color={titleColor}
-                        as={FaApple}
-                        w='30px'
-                        h='30px'
-                        _hover={{ filter: "brightness(120%)" }}
-                      />
-                    </Link>
-                  </Flex>
-                </GradientBorder>
-                <GradientBorder borderRadius='15px'>
-                  <Flex
-                    _hover={{ filter: "brightness(120%)" }}
-                    transition='all .25s ease'
-                    cursor='pointer'
-                    justify='center'
-                    align='center'
-                    bg='rgb(19,21,54)'
-                    w='71px'
-                    h='71px'
-                    borderRadius='15px'>
-                    <Link href='#'>
-                      <Icon
-                        color={titleColor}
-                        as={FaGoogle}
-                        w='30px'
-                        h='30px'
-                        _hover={{ filter: "brightness(120%)" }}
-                      />
-                    </Link>
-                  </Flex>
-                </GradientBorder>
-              </HStack>
-              <Text
-                fontSize='lg'
-                color='gray.400'
-                fontWeight='bold'
-                textAlign='center'
-                mb='22px'>
-                or
-              </Text>
-              <FormControl>
-                <FormLabel
-                  color={titleColor}
-                  ms='4px'
-                  fontSize='sm'
-                  fontWeight='normal'>
-                  Name
-                </FormLabel>
-
-                <GradientBorder
-                  mb='24px'
-                  h='50px'
-                  w={{ base: "100%", lg: "fit-content" }}
-                  borderRadius='20px'>
-                  <Input
-                    color={titleColor}
-                    bg={{
-                      base: "rgb(19,21,54)",
-                    }}
-                    border='transparent'
-                    borderRadius='20px'
-                    fontSize='sm'
-                    size='lg'
-                    w={{ base: "100%", md: "346px" }}
-                    maxW='100%'
-                    h='46px'
-                    type='text'
-                    placeholder='Your name'
-                  />
-                </GradientBorder>
-                <FormLabel
-                  color={titleColor}
-                  ms='4px'
-                  fontSize='sm'
-                  fontWeight='normal'>
-                  Email
-                </FormLabel>
-                <GradientBorder
-                  mb='24px'
-                  h='50px'
-                  w={{ base: "100%", lg: "fit-content" }}
-                  borderRadius='20px'>
-                  <Input
-                    color={titleColor}
-                    bg={{
-                      base: "rgb(19,21,54)",
-                    }}
-                    border='transparent'
-                    borderRadius='20px'
-                    fontSize='sm'
-                    size='lg'
-                    w={{ base: "100%", md: "346px" }}
-                    maxW='100%'
-                    h='46px'
-                    type='email'
-                    placeholder='Your email address'
-                  />
-                </GradientBorder>
-                <FormLabel
-                  color={titleColor}
-                  ms='4px'
-                  fontSize='sm'
-                  fontWeight='normal'>
-                  Password
-                </FormLabel>
-                <GradientBorder
-                  mb='24px'
-                  h='50px'
-                  w={{ base: "100%", lg: "fit-content" }}
-                  borderRadius='20px'>
-                  <Input
-                    color={titleColor}
-                    bg={{
-                      base: "rgb(19,21,54)",
-                    }}
-                    border='transparent'
-                    borderRadius='20px'
-                    fontSize='sm'
-                    size='lg'
-                    w={{ base: "100%", md: "346px" }}
-                    maxW='100%'
-                    h='46px'
-                    type='password'
-                    placeholder='Your password'
-                  />
-                </GradientBorder>
-                <FormControl display='flex' alignItems='center' mb='24px'>
-                  <DarkMode>
-                    <Switch id='remember-login' colorScheme='brand' me='10px' />
-                  </DarkMode>
-
-                  <FormLabel
-                    color={titleColor}
-                    htmlFor='remember-login'
-                    mb='0'
-                    fontWeight='normal'>
-                    Remember me
-                  </FormLabel>
-                </FormControl>
-                <Button
-                  variant='brand'
-                  fontSize='10px'
-                  type='submit'
-                  w='100%'
-                  maxW='350px'
-                  h='45'
-                  mb='20px'
-                  mt='20px'>
-                  SIGN UP
-                </Button>
-              </FormControl>
-              <Flex
-                flexDirection='column'
-                justifyContent='center'
-                alignItems='center'
-                maxW='100%'
-                mt='0px'>
-                <Text color={textColor} fontWeight='medium'>
-                  Already have an account?
-                  <Link
-                    color={titleColor}
-                    as='span'
-                    ms='5px'
-                    href='#'
-                    fontWeight='bold'>
-                    Sign In
-                  </Link>
-                </Text>
-              </Flex>
-            </Flex>
-          </GradientBorder>
-        </Flex>
-        <Box
-          w={{ base: "335px", md: "450px" }}
-          mx={{ base: "auto", lg: "unset" }}
-          ms={{ base: "auto", lg: "auto" }}
-          mb='90px'>
-          <AuthFooter />
-        </Box>
-        <Box
-          display={{ base: "none", lg: "block" }}
-          overflowX='hidden'
-          h='1300px'
-          maxW={{ md: "50vw", lg: "48vw" }}
-          w='960px'
-          position='absolute'
-          left='0px'>
-          <Box
-            bgImage={signUpImage}
-            w='100%'
-            h='1300px'
-            bgSize='cover'
-            bgPosition='50%'
-            position='absolute'
-            display='flex'
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='center'
-            position='absolute'>
-            <Text
-              textAlign='center'
-              color='white'
-              letterSpacing='8px'
-              fontSize='20px'
-              fontWeight='500'>
-              INSPIRED BY THE FUTURE:
-            </Text>
-            <Text
-              textAlign='center'
-              color='transparent'
-              letterSpacing='8px'
-              fontSize='36px'
-              fontWeight='bold'
-              bgClip='text !important'
-              bg='linear-gradient(94.56deg, #FFFFFF 79.99%, #21242F 102.65%)'>
-              THE VISION UI DASHBOARD
-            </Text>
-          </Box>
-        </Box>
-      </Flex>
     </Flex>
   );
 }
 
 export default SignUp;
-  );
-}
-
-export default SignUp;
+          
