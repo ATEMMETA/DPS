a0f47ea40a00d1565ffd4c68d72267e24467bfa9
ATEMMETA <atem.86796@gmail.com>
2025-03-27 13:59:43 -0500

    Update CodeBlock.tsx

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -6,7 +6,7 @@ import { FC, useState } from 'react';
 import { DndProvider, useDrag, useDrop, ConnectDropTarget } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-yinterface Props {
+interface Props {
   code: string;
   height: string;
   editable?: boolean;
