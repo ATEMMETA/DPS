a833a15c888d2085781733cfc1487ed398c8a142
ATEMMETA <atem.86796@gmail.com>
2025-03-30 16:47:55 -0500

    Update authfooter.tsx

--- src/components/footer/authfooter.tsx
+++ src/components/footer/authfooter.tsx
@@ -1,6 +1,6 @@
 import { Box, Text, Link } from "@chakra-ui/react";
 
-const AuthFooter = () => (
+const authfooter = () => (
   <Box textAlign="center" mt="20px">
     <Text fontSize="xs" color="gray.400">
       © {new Date().getFullYear()} Your App. All rights reserved.
@@ -11,4 +11,4 @@ const AuthFooter = () => (
   </Box>
 );
 
-export default AuthFooter;
+export default authfooter;
