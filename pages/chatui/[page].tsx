90f576d1aa40c87eaee5cc2798b3c4407018c269
ATEMMETA <atem.86796@gmail.com>
2025-04-03 18:26:39 -0500

    Update [page].tsx

--- pages/chatui/[page].tsx
+++ pages/chatui/[page].tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation'; // Fixed import
 import { useState } from 'react';
 import { Box, Input, Button, VStack, Text, Heading } from '@chakra-ui/react';
 
