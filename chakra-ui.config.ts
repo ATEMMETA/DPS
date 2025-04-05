62dc6f13a84be43b602d10c9f4445a362e5f8238
ATEMMETA <atem.86796@gmail.com>
2025-04-04 20:38:48 -0500

    Create chakra-ui.config.ts

---
+++ chakra-ui.config.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      500: '#4A25E1', // Map your hex colors
+      600: '#7B5AFF',
+    },
+  },
+});
+
+export default theme;
