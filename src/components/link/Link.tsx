7936596cd006f9f7f855472a26c871dbddade6ac
ATEMMETA <atem.86796@gmail.com>
2025-04-10 21:30:45 -0500

    Update Link.tsx

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,19 +1,33 @@
 'use client';
-import { ButtonProps } from '@chakra-ui/react';
-import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+import { Box, Flex, Icon, Text, useColorModeValue } from '@chakra-ui/react';
+import NavLink from '@/components/link/NavLink';
+import { IRoute } from '@/types/navigation';
 
-import { Button } from '@chakra-ui/react';
-
-type LinkProps = ButtonProps & NextLinkProps;
-
-function Link({ href, children, ...props }: LinkProps) {
-  return (
-    <NextLink href={href} passHref legacyBehavior>
-      <Button as="a" variant="a" {...props}>
-        {children}
-      </Button>
-    </NextLink>
-  );
+interface LinksProps {
+  routes: IRoute[];
 }
 
-export default Link;
+export default function Links({ routes }: LinksProps) {
+  const textColor = useColorModeValue('navy.700', 'white');
+  const inactiveColor = useColorModeValue('gray.500', 'gray.400');
+
+  return (
+    <Box>
+      {routes.map((route, index) => {
+        if (route.invisible || route.disabled) return null; // Skip invisible/disabled
+        return (
+          <NavLink key={index} href={route.path}>
+            <Flex align="center" p="10px" borderRadius="md" _hover={{ bg: 'gray.100' }}>
+              {route.icon && (
+                <Icon as={route.icon.type} w="20px" h="20px" color={inactiveColor} mr="10px" />
+              )}
+              <Text color={textColor} fontSize="sm" fontWeight="500">
+                {route.name}
+              </Text>
+            </Flex>
+          </NavLink>
+        );
+      })}
+    </Box>
+  );
+}
